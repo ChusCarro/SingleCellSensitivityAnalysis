fe330cf3f636cfb5c9b2bd9718a5c4a1f0a59eb9
Jesús Carro <chuscarro@gmail.com>
2015-03-24 10:40:35 +0100

    Updated the TP06 example

--- examples/TP06.m
+++ examples/TP06.m
@@ -2,7 +2,6 @@ addpath([pwd() '/../src'])
 
 pathToSave = '~/SingleCellSensitivityResults/TP06';
 mainElvira = '~/Software/TestEM/TestEM20150303/bin/testem_gcc';
-project = 'Conductance Sensitivity - TP06 Model';
 
 param = [1:13];
 values =[0.70 0.85 1.15 1.30];
@@ -11,13 +10,13 @@ cores=4;
 dt = 0.02;
 step_save=5;
 %[s]=rmdir(Model,'s');
-Imax = 500;
-Istep = 1;
+Imax = 100;
+Istep = 0.1;
 
 CL = 1000;
 nCLs = 300;
 
-SensitivityAnalysis(cores, pathToSave, mainElvira, project, cellType, param, values, dt,...
+SensitivityAnalysis(cores, pathToSave, mainElvira,cellType, param, values, dt,...
              step_save, Imax, Istep, CL, nCLs)
 
 %plotIThresholdLimits(pathToSave)
